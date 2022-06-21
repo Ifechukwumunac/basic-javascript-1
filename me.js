fdf4a19f2a8d4746e49a9933f19506ae82ab5ac7
munachiso <ifechukwumunachiso5@gmail.com>
2022-06-21 02:15:09 -0700

    initial commit

---
+++ me.js
@@ -0,0 +1,10 @@
+// please note one can only validate bt not restrict user input with the prompt
+    let firstName  = prompt('please Enter your first name');
+    lastName  = prompt('please enter your last name');
+    height = prompt('How tall are you? \n please specify with the unit of measurement!');
+    country = prompt('Which county are you from?')
+
+alert("Your name is " + firstName +" "+ lastName + "\n your height is " + height + "\n and you are from " +  country + ".")
+
+document.write("Your name is " + firstName + " "+ lastName + "\n your height is " + height + "\n and you are from" +  country + ".")
+
